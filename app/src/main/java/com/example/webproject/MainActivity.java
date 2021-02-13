20dc7356b5a649b2c95ec575af473ad101a09733
Pronobesh-Coder <pronobesh.chatterjee@theheritageschool.net.in>
2021-02-13 19:36:52 +0530

    Web_Project

---
+++ app/src/main/java/com/example/webproject/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.webproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.web_view);
+        webView.loadUrl("https://www.google.com");
+    }
+}
